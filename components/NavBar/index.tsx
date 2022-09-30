81272e3110c272ed495b936ef9094a58e96026a0
Alexey Mikhaylov <alexeym.vn@gmail.com>
2022-09-30 22:05:47 +0300

    Add ui library, add basic navbar

---
+++ components/NavBar/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import type { MenuItem } from 'primereact';
+import { Button } from 'primereact/button';
+import { Menubar } from 'primereact/menubar';
+import { useMemo } from 'react';
+import { useUser } from '../../context/user';
+
+
+
+export const NavBar = () => {
+  const { user, isLoading, logout, loginWithGoogle } = useUser();
+  const router = useRouter();
+  
+  const items: MenuItem[] = [
+    {
+      label: 'Goals',
+      command: () => router.push('/goals')
+    },
+    {
+      label: 'People',
+      command: () => router.push('/people')
+    }
+  ]
+
+
+  const end = isLoading ? null : user ?  <Button onClick={logout} className="p-button-text">Logout</Button> : <Button onClick={loginWithGoogle}>Login</Button>
+  return (
+    <Menubar
+      model={items}
+      end={
+       end
+      }
+    />
+  )
+}
